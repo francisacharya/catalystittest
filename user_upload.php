d4b9d6bff633c2c0aa01cbbf812b8c865df273ea
francis acharya <francis.acharya@subscribe-hr.com.au>
2022-07-08 21:59:39 +1000

    implementing array slice instead of a loop

--- user_upload.php
+++ user_upload.php
@@ -1,33 +1,17 @@
 <?php
-//read a file
-if (($open = fopen("users.csv", "r")) !== FALSE) 
-  {
-  
-    while (($data = fgetcsv($open, 1000, ",")) !== FALSE) 
-    {        
-      $array[] = $data; 
-    }
-  
-    fclose($open);
-  }
-  
-//To display array data
-//var_dump($array);
-//process the array
-foreach ($array as $key => $value) {
-    //echo "{$key} => {$value} ";
-    if($key!=0)
-    {
-        //removing first entry
-        $requiredArray[$key] = $value;
-    }
-   
-}
+/**
+
+ * @param Arr $single 
+
+ * @return Arr $finalArray 
+
+ */
 function processVals($single)
 {
     //var_dump($single); die();
     foreach ($single as $key => $singleValue) {
         //var_dump($singleValue);die();
+        //converting firstname and last name's first letter to uppercase and email in lowercase
         if($key == 0 || $key == 1){
             $finalArray[$key] = ucfirst($singleValue);
         }
@@ -44,15 +28,29 @@ function processVals($single)
     return $finalArray;
     
 }
+//read a file
+if (($open = fopen("users.csv", "r")) !== FALSE) 
+{
+
+while (($data = fgetcsv($open, 1000, ",")) !== FALSE) 
+{        
+    $array[] = $data; 
+}
+
+    fclose($open);
+}
+  
+//To display array data without headings
+
+$requiredArray = array_slice($array, 1);
 
 foreach ($requiredArray as $key => $singleValue) {
-    //converting firstname and last name's first letter to uppercase and email in lowercase
     //var_dump($singleValue);die();
    $processedArr[$key] = processVals($singleValue);
 }
 print_r($processedArr);die();
 //db connection , this is to be done in seperate file
-// data processing
+// data processing before insert to db
 //data insertion to db
 
 ?>
